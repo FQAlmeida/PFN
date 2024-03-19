d1ef0ae334cfadcb0ce458d553e68230427198d3
Otávio Almeida <otavioalmeida650@gmail.com>
2024-03-19 16:41:52 -0300

    aula 5

---
+++ aula5.hs
@@ -0,0 +1,23 @@
+fib :: (Num a, Eq a) => a -> a
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n -1) + fib (n -2)
+
+memoizedFib :: Int -> Integer
+memoizedFib = (map fib [0 ..] !!)
+  where
+    fib 0 = 0
+    fib 1 = 1
+    fib n = memoizedFib (n -2) + memoizedFib (n -1)
+
+soma :: (Eq p, Num p) => p -> p
+soma 0 = 0
+soma n = n + soma (n - 1)
+
+somaEntre :: (Eq p, Num p, Ord p) => p -> p -> p
+somaEntre a b | a >= b = 0
+somaEntre a b = a + somaEntre (a + 1) b
+
+seqRoot :: (Eq t, Floating p, Num t) => t -> p
+seqRoot 1 = sqrt 6
+seqRoot n = sqrt (6 + seqRoot (n - 1))
